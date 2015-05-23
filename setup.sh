fe9ec1585958baf7dae3480c46f1c41963b473f2
Tadayuki Onishi <tt.tanishi100@gmail.com>
2015-05-24 01:31:58 +0900

    change to use ssh https when git clone in setup.sh

--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ do
 done
 
 # install oh-my-zsh
-[ ! -d $HOME/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+[ ! -d $HOME/.oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
 # install Neobundle
-[ ! -d $HOME/.vim/bundle/neobundle.vim ] && mkdir -p $HOME/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
+[ ! -d $HOME/.vim/bundle/neobundle.vim ] && mkdir -p $HOME/.vim/bundle && git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
 
 [ ! `grep 'gitignore_global' $HOME/.gitconfig` ] && git config --global core.excludesfile $HOME/.gitignore_global
