3b641ffa78d848d21f46f117e883e97275e1e091
Tadayuki Onishi <tt.tanishi100@gmail.com>
2015-05-23 17:21:53 +0900

    update setup shell

--- setup.sh
+++ setup.sh
@@ -17,6 +17,6 @@ done
 [ ! -d $HOME/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
 # install Neobundle
-[ ! -d $HOME/.vim/bundle ] && mkdir -p $HOME/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
+[ ! -d $HOME/.vim/bundle/neobundle.vim ] && mkdir -p $HOME/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
 
 [ ! `grep 'gitignore_global' $HOME/.gitconfig` ] && git config --global core.excludesfile $HOME/.gitignore_global
