4d4c27141eb2f9772e0004fb7458af42924e2f89
Tadayuki Onishi <tt.tanishi100@gmail.com>
2016-06-21 09:55:10 +0900

    support iTerm 3.x

--- home/.config/fish/settings/osx.fish
+++ home/.config/fish/settings/osx.fish
@@ -34,11 +34,11 @@ if begin; type convert >/dev/null 2>&1; and test -d /Applications/iTerm.app; end
             set image_path $image_list[$image_index]
             convert $image_path -resize "$height"x"$width" -size "$height"x"$width" xc:"#353535" +swap -gravity center -composite $display_image_path
             osascript -e "tell application \"iTerm\"
-            set current_terminal to (current terminal)
-            tell current_terminal
+            set current_window to (current window)
+            tell current_window
             set current_session to (current session)
             tell current_session
-            set background image path to \"$display_image_path\"
+            set background image to \"$display_image_path\"
             end tell
             end tell
             end tell"
