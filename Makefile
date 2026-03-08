bb38514885a9c48f1e5d4dd8a7c9472c5cfbde32
kenchan0130 <1155067+kenchan0130@users.noreply.github.com>
2026-03-08 23:30:57 +0900

    add shells

---
+++ Makefile
@@ -0,0 +1,9 @@
+defaults:
+	@echo "\033[0;34mRun defaults.sh\033[0m"
+	@.bin/defaults.sh
+	@echo "\033[0;32mDone.\033[0m"
+
+brew:
+	@echo "\033[0;34mRun brew.sh\033[0m"
+	@.bin/brew.sh
+	@echo "\033[0;32mDone.\033[0m"
---
+++ dot_bin/executable_defaults.sh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+
+# Show files with all extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Use the Fn key as a standard function key
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
+# Key repeat speed
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+# Timing to start key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 25
+
+# Disable auto-capitalization
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Do not create .DS_Store files on USB or network storage
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable waiting time before the Dock is displayed
+defaults write com.apple.dock autohide-delay -float 0
+
+# Show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Display the status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Display the path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Apply soon
+for app in "Dock" \
+	"Finder" \
+	"SystemUIServer"; do
+	killall "${app}" &> /dev/null
+done
